e222e306dd6ccfbbf86b2b2fa7a7c8aaf477b044
voidnoble <cezips@gmail.com>
2020-10-26 16:00:43 +0900

    0.6 Interfaces on Typescript

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+};
+
+const person = {
+  name: "jun",
+  age: 24,
+  gender: "male"
+};
+
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+
+console.log(sayHi(person));
+
+export {};
